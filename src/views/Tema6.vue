2623f564d2e559090a21bc897895462605cfbdde
lizmanchego <karinamanchego0416@gmail.com>
2024-11-21 09:44:04 -0500

    [MAQ] Tema 6 Full

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,32 +5,82 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 6
-      h1 Embalaje de los documentos
+      h1 Elementos de archivo 
     
-    .row
-      .col-lg-3.d-none.d-lg-block(data-aos="fade-right")
-        img(src="@/assets/curso/temas/t6/img1.png", alt="")
-      .col-lg-9(data-aos="fade-left")
-        .tarjeta.bgi-2.h-100.p-4.p-lg-5
-          .tarjeta.tarjeta--blanca.py-4
-            SlyderA.sin-botones
-              .row.justify-content-center
-                .col-md-4.col-8.mb-4.mb-md-0
-                  img(src="@/assets/curso/temas/t6/slyder1.svg", alt="")            
-                .col-lg-6.col-md-9.col-8
-                  h5 El empaque
-                  p Suele ser utilizado generalmente en las estrategias de marketing para garantizar la calidad de un producto. Pero cuando se habla de la gestión documental se entiende por empaque al sobre, envoltura, plastificación o cubierta en otro tipo de material, donde se guarda el documento al momento de su despacho para protegerlo hasta el momento de la entrega al destinatario.
-              .row.justify-content-center
-                .col-lg-10.col-md-9.col-8
-                  h5 El embalaje
-                  p Generalmente suele asociarse para cantidades más grandes de documentos o acervos que requieren ser trasladados, buscando conservar sus propiedades antes de iniciar el recorrido, durante el mismo y al final de su entrega. Se diligencia un formato con los datos más relevantes del documento, generalmente se traza un diseño específico según la empresa y los criterios documentales que se tengan en ella. Cuando la correspondencia está orientada a dar una respuesta, no sólo se registran los criterios de su salida o despacho sino que se anota también el consecutivo y fecha del documento que antecede a este; esta operación constituye el control y el trámite documental.
-              .row.justify-content-center
-                .col-lg-10.col-md-9.col-8
-                  h5 El envío
-                  p En primera medida se necesita tener claridad sobre la forma de envío que se desea realizar, definiendo el nivel geográfico de su traslado y las condiciones de empaque y seguridad; estos procedimientos deben garantizar que el documento llegue a su destino final sin sufrir alteraciones en su contenido o esencia. Además, se debe escoger la mejor forma de empaque, la empresa que prestará el servicio y la verificación de tarifas que implica realizar esta gestión; ya que, se debe ser consciente que acudir a este medio, requiere tener un fondo que solvente los costos de la unidad de correspondencia.
-    
-    
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-6
+        LineaTiempoD.color-acento-contenido.mb-cus
+          .row(numero="1" titulo="Archivadores")
+            .col-12
+              p.mb-0 Muebles que facilitan la ubicación, búsqueda y consulta de información. Entre ellos se encuentran los siguientes.
+          .row(numero="2" titulo="Archivador vertical")
+            .col-12
+              p.mb-0 Se colocan junto al escritorio en oficinas pequeñas para conservación y facilitar la consulta de los documentos correspondientes al archivo de gestión. Tiene de una a cinco gavetas.
+          .row(numero="3" titulo="Archivador horizontal")
+            .col-12
+              p.mb-0 Facilita clasificar los documentos de forma horizontal, pero se encuentra generalmente cerrado. Las carpetas que se utilizan se denominan carpetas colgantes.
+          .row(numero="4" titulo="Folderama")
+            .col-12
+              p.mb-0 Facilita la conservación de los documentos en carpetas con guía lateral. Se utilizan en las organizaciones para guardar las historias laborales.
+          .row(numero="5" titulo="Archivador giratorio")
+            .col-12
+              p.mb-0 Este archivador está constituido por hileras circulares. Además, están equipados con teléfonos para que los responsables del área de trabajo suministren la información requerida.
+          .row(numero="6" titulo="Archivador mecánico")
+            .col-12
+              p.mb-0 Es un mueble dinámico, en el cual se acopia gran cantidad de documentos de acuerdo a la clasificación interna de la organización. Debido a su condición física permite de una manera práctica la búsqueda y manipulación de la información contenida en los documentos.
+          .row(numero="7" titulo="Archivador electrónico")
+            .col-12
+              p.mb-0 Es una herramienta de tipo ofimática que sirve para recopilar la información de una manera ordenada utilizando dispositivos ópticos, para su localización y enlace en las debidas carpetas.
+          .row(numero="8" titulo="Estantes")
+            .col-12
+              p.mb-0 Es una estructura metálica o de manera horizontal que tiene como propósito de garantizar la conservación de la información de los documentos.
+          .row(numero="9" titulo="Archivador rodante")
+            .col-12
+              p.mb-0 Es un conjunto de estanterías metálicas agrupadas con ruedas, que implementa un sistema versátil de almacenamiento hermético de los documentos para mayor accesibilidad y conservación de la información.
+          .row(numero="10" titulo="Planoteca")
+            .col-12
+              p.mb-0 Es una estructura en madera que tiene como función almacenar planos y facilitar su consulta.
+          
+      .col-sm-12.col-lg-6
+        img(src="@/assets/curso/temas/t6/1.svg", alt="").mb-5
+        LineaTiempoD.color-acento-contenido
+          .row(numero="11" titulo="Biblioteca")
+              .col-12
+                p.mb-0 Según el archivo general de la nación: “es la recepción, selección, ordenamiento, fichaje, custodia y conservación del material bibliográfico, facilitando la consulta de los investigadores y al público en general”. 
+                p.mb-0 #[b Fototeca:] es un organismo que se encarga de recolectar, conservar y consulta de documentos fotográficos. 
+                p.mb-0 #[b Mapoteca:] es aquel lugar en el cual se guarda y conserva todo tipo de mapas. 
+                p.mb-0 #[b Hemeroteca:] es una estructura que se especializa en guardar, conservar y consultar diarios, publicaciones, folletos y boletines, los cuales se pueden clasificar por tema, país y fecha.
 
+          .row(numero="12" titulo="Sello Fechador")
+              .col-12
+                p.mb-0 Es un elemento con imágenes grabadas y piezas móviles que permiten modificar la fecha para marcarla sobre la superficie.
+          .row(numero="13" titulo="Gancho Seco o en relieve")
+              .col-12
+                p.mb-0 Es una pieza metálica compuesta por grabados. Su función es marcar los documentos e identificar y protegerlos de las falsificaciones.
+          .row(numero="14" titulo="Reloj Radicador")
+              .col-12
+                p.mb-0 El reloj radicador es utilizado en la unidad de correspondencia, para la recepción de las comunicaciones, facilitando y agilizando la radicación. La organización documental precisa de un control que permita comprobar el recibo y trámite de documentos generados por la organización, o que han sido allegados a esta; evitando la evasión de responsabilidades por parte de las personas encargadas del recibo y despacho de documentos.
+          .row(numero="15" titulo="Gancho legajador plástico")
+              .col-12
+                p.mb-0 Es un gancho plástico que permite fijar los documentos que se encuentran contenidos en un legajo manteniendo su conservación y evitando deterioro por oxidación.
+          .row(numero="16" titulo="Legajos ")
+              .col-12
+                p.mb-0 Son dos tapas de cartulina con gancho legajador, para asegurar los documentos, preferiblemente en materia desacidificada, para evitar el deterioro de los documentos.
+          .row(numero="17" titulo="Cajas de Archivo ")
+              .col-12
+                p.mb-0 Estas cajas son prácticas para organizar en estanterías. Los documentos reposan en ellas y sus cierres son laterales. Su registro en ellas facilita la búsqueda de la información, además de la protección y conservación interna.
+          .row(numero="18" titulo="Papelería ")
+              .col-12
+                p.mb-0 Los papeles más usados para la documentación son: 
+                p.mb-0 #[b Tamaño Carta:] empleado regularmente para las comunicaciones internas y externas de la organización (clientes, proveedores). 
+                p.mb-0 #[b Tamaño Oficio:] empleado normalmente por su capacidad para la realización de documentos de carácter jurídico (acuerdos, contratos, resoluciones). 
+                p.mb-0 #[b Bond de 75 gr:] generalmente empleado para fotocopias.
+          .row(numero="19" titulo="Instrumentos de medición en los archivos")
+              .col-12
+                p.mb-0 Los siguientes aparatos son empleados para garantizar la calidad y conservación en los documentos y demás soportes documentales que reposan en los archivos. 
+                p.mb-0 #[b Termómetro:] Busca identificar la temperatura ambiente y las condiciones climáticas de las operaciones en el recinto.
+                p.mb-0 #[b Higroscopio o Higrómetro:] Estos aparatos miden tanto la temperatura como la humedad, al mismo tiempo. En la actualidad son sofisticadas máquinas prácticas de tipo digital.
+              
 </template>
 
 <script>
