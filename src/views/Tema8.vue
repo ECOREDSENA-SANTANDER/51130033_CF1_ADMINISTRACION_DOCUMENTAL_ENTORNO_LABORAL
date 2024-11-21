eebcb57f094d5db7d973d4ca18a3a8a38f8d2d5a
lizmanchego <karinamanchego0416@gmail.com>
2024-11-21 10:49:18 -0500

    [MAQ] Tema 8: 8.1/8.2

--- src/views/Tema8.vue
+++ src/views/Tema8.vue
@@ -5,9 +5,120 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 8
-      h1 Elementos de archivo 
+      h1 Importancia, clasificación y tipología de los documentos.
+    p.mb-3 El documento es el principal elemento de un archivo, ya que contiene información que puede ser requerida posteriormente, debido a la necesidad que tiene la empresa para el desarrollo de su gestión, objetivos y, sobre todo, para el servicio al cliente interno y externo, ya que la información que contienen los documentos permite tomar decisiones basadas en antecedentes. 
+    p.mb-5 Los documentos permiten que las actividades propias de una empresa se puedan ejecutar adecuadamente en cada una de sus áreas, y una vez cumplida la función, constituyan un archivo sujeto a las disposiciones legales. Por ello, se hace oportuno mencionar los aspectos más significativos del papel que cumple un documento, como elemento clave que resalta su importancia:
+    .row.justify-content-center.mb-5
+      .col-sm-12.col-lg-6
+        ol.lista-ol--cuadro.lista-ol--separador
+          li.d-flex
+            .lista-ol--cuadro__vineta
+              span 1
+            span.mb-0  Transmiten información y sirven como evidencia de las gestiones desarrolladas a través de sus diferentes formas de soporte, en ejercicio de las funciones administrativas. 
+          li.d-flex
+            .lista-ol--cuadro__vineta
+              span 2
+            span.mb-0 Constituyen un medio de comunicación e interacción humana. 
+      .col-sm-12.col-lg-6
+        ol.lista-ol--cuadro.lista-ol--separador
+          li.d-flex
+            .lista-ol--cuadro__vineta
+              span 3
+            span.mb-0 Sirven de testimonio a las gestiones administrativas que lo requieran. 
+          li.d-flex
+            .lista-ol--cuadro__vineta
+              span 4
+            span.mb-0 Intervienen directamente en el proceso de la toma de decisiones. 
+          li.d-flex
+            .lista-ol--cuadro__vineta
+              span 5
+            span.mb-0 Soportan procesos legales personales e institucionales.
     
-   
+    
+    
+    Separador
+    #t_8_1.titulo-segundo.color-acento-contenido
+      h2 8.1 Según el Soporte
+    p.mb-5 Los documentos de archivo se clasifican según la función que cumplen, y el impacto que buscan generar en el destino o destinatarios; su principal objetivo radica en la rápida y efectiva localización de la información que contienen, a partir de los conceptos que los agrupa como reflejo de sus funciones generales, lo que les concede una estructura jerárquica y lógica.
+    .row.bgc-1.align-items-center.p-4.py-5.p-lg-5
+      .col-lg-12.col-12.px-lg-5.px-4
+        .row.justify-content-center.align-items-center
+          .tarjeta.tarjeta--blanca.p-4.p-lg-5
+            SlyderA(tipo="b")
+              .row.justify-content-center.align-items-center
+                .col-sm-12.col-lg-9
+                  .card-title-cus.mb-5
+                    h4.mb-0.py-3.px-4 Según el Soporte
+                  p Los documentos de archivo se clasifican según la función que cumplen, y el impacto que buscan generar en el destino o destinatarios; su principal objetivo radica en la rápida y efectiva localización de la información que contienen, a partir de los conceptos que los agrupa como reflejo de sus funciones generales, lo que les concede una estructura jerárquica y lógica.
+                  p Se clasifican por sus características físicas y condiciones de conservación, así:
+
+                  p #[b Textuales:] esta clase de documentos son los que más se utilizan en las empresas, algunos ejemplos de ellos son los manuscritos como; memorandos, circulares, cartas, informes. 
+                  p #[b Gráficos:] permiten visualizar de manera rápida la información, y el modelo empleado depende de la actividad o requerimiento específico. Los más utilizados son los mapas, dibujos y planos. 
+                  p #[b Imagen:] reflejan y/o evidencian la información plasmada en textos o gráficos, por lo general, se realiza a través de cuadros, fotografías y diapositivas, entre otros. 
+                  p #[b Cinta:] son aquellos medios audiovisuales que buscan, de una manera dinámica, motivar, plasmar y captar la atención de una o varias personas, como los videos, pizarrón magnético, microfilm, cintas magnéticas, entre otros. Otros Soportes: entre ellos se mencionan algunos como los discos duros, material óptico y sellos.
+                .col-sm-12.col-lg-3.d-none.d-lg-block
+                    img(src="@/assets/curso/temas/t8/2.svg", alt="")
+              .row.justify-content-center.align-items-center
+                .col-sm-12.col-lg-9
+                  .card-title-cus.mb-5
+                    h4.mb-0.py-3.px-4 Según el Productor o Autor 
+                  p Se refiere a la empresa o persona que elabora el documento y la función a la cual se destina. Se clasifican en:
+                  p #[b Documento Público:] son los documentos producidos o recibidos por un funcionario o ente del estado, es decir, aquel instrumento que se tramita ante un funcionario público, que cumple una función que atañe directamente al Estado y tiene carácter representativo.
+                  p #[b Documento Privado:] son aquellos documentos producidos o recibidos por entidades privadas o personas naturales.
+
+                .col-sm-12.col-lg-3.d-none.d-lg-block   
+                  img(src="@/assets/curso/temas/t8/3.svg", alt="")
+              .row.justify-content-center.align-items-center
+                .col-sm-12.col-lg-9
+                  .card-title-cus.mb-5
+                    h4.mb-0.py-3.px-4 Según su Finalidad
+                  p Cuando los documentos tienen una función con un objetivo que deben cumplir, pero que considera la circunstancia que hace que se genere la información que esté soportando. Su correspondiente clasificación.
+                  p #[b Documentos Dispositivos:] aquellos que imparten normas de obligatorio cumplimiento: leyes, decretos, ordenanzas, acuerdos, resoluciones. 
+                  p #[b Documentos Informativos:] aquellos que conforman la generalidad de los documentos dentro de la función administrativa de una empresa, como son: cartas, memorando, circulares, presupuestos.
+                  p #[b Documentos Testimoniales:] aquellos que sirven como prueba o testimonio, un ejemplo son como los documentos contables, contratos, etc.
+
+                .col-sm-12.col-lg-3.d-none.d-lg-block   
+                  img(src="@/assets/curso/temas/t8/4.svg", alt="")
+              .row.justify-content-center.align-items-center
+                .col-sm-12.col-lg-9
+                  .card-title-cus.mb-5
+                    h4.mb-0.py-3.px-4 Según su Difusión en el Tiempo
+                  p Es importante tener en cuenta, que no todos los documentos tienen una vigencia indefinida, sino que han sido diseñados exclusivamente para cumplir una función que tiene un principio y un final en el tiempo, como ocurre con la Tabla de Retención Documental, en la que se encuentra establecido el tiempo de permanencia de los documentos en cada fase del archivo, y su disposición final, ya se de conservación o eliminación.
+                .col-sm-12.col-lg-3.d-none.d-lg-block   
+                  img(src="@/assets/curso/temas/t8/5.svg", alt="")
+              .row.justify-content-center.align-items-center
+                .col-sm-12.col-lg-9
+                  .card-title-cus.mb-5
+                    h4.mb-0.py-3.px-4 Según su Funcionalidad
+                  p Los documentos se crean con el ánimo de apoyar una gestión, a continuación, se presenta su clasificación.
+                  p #[b Sustantivos o Específicos:] aquellos que soportan las funciones y objetivos de la empresa en sus diferentes áreas, por ejemplo, la programación de horarios de atención, rotación de turnos, agenda de vuelos, ruteros y demás.
+                  p #[b Facilitativos o Comunes:] aquellos que además de apoyar la gestión empresarial, también se hacen presentes con contenidos diferentes, pero cumpliendo las mismas funciones, por ejemplo las nóminas, los libros contables, hojas de vida y control de expedientes del personal.
+
+                .col-sm-12.col-lg-3.d-none.d-lg-block   
+                  img(src="@/assets/curso/temas/t8/6.svg", alt="")
+
+
+    
+    
+    Separador
+    #t_8_2.titulo-segundo.color-acento-contenido
+      h2 8.2 Según el Productor o Autor
+    p.mb-0 La tipología documental es la unidad documental simple originada de una actividad administrativa, en cumplimiento de las funciones o actividades de la empresa.
+    p Ejemplos de tipo documental son: resoluciones, memorandos, informes, entre otros.
+    p La tipología documental expresa de manera clara la actuación y toma de decisiones por parte de la administración, decisiones que se ven reflejadas en un determinado soporte —papel, microfilm, medio electrónico, entre otros.
+    p Generalmente, los tipos documentales de un mismo soporte, acude por estructura, a caracteres internos específicos a cada uno de ellos determinando su contenido. Es importante considerar, que los tipos documentales se derivan propiamente de cada una de las instancias o dependencias que los generan dentro de cada institución, lo que implica de manera práctica, que cada función produce uno o varios tipos de documentos.
+
+    Separador
+    #t_8_3.titulo-segundo.color-acento-contenido
+      h2 8.3 Según su Finalidad
+    
+    Separador
+    #t_8_4.titulo-segundo.color-acento-contenido
+      h2 8.4 Según su Difusión en el Tiempo
+    
+    Separador
+    #t_8_5.titulo-segundo.color-acento-contenido
+      h2 8.5 Según su Funcionalidad
 </template>
 
 <script>
